cdad689dec077983a119b88eaa523bc185296e13
Tim Hobson <thobson@turing.ac.uk>
2019-11-13 22:10:25 +0000

    Add unit tests for Poisson scenario generation Includes a bugfix: removal of numpy for exponential random variable sampling so the random seed is respected throughout

--- aviary/scenario/poisson_scenario.py
+++ aviary/scenario/poisson_scenario.py
@@ -4,22 +4,18 @@ Scenario generation algorithm with Poisson aircraft arrivals.
 # author: Tim Hobson
 # email: thobson@turing.ac.uk
 
-# import os.path
-import time
-import numpy as np
+import random
 
 from aviary.scenario.scenario_algorithm import ScenarioAlgorithm
 import aviary.scenario.scenario_generator as sg
-
-# import aviary.sector.sector_element as se
 
 class PoissonScenario(ScenarioAlgorithm):
     """A Poisson scenario generator for I, X, Y airspace sectors"""
 
 
-    def __init__(self, arrival_rate, seed = None):
+    def __init__(self, arrival_rate, **kwargs):
 
-        super().__init__(seed = seed)
+        super().__init__(**kwargs)
         self.arrival_rate = arrival_rate
 
 
@@ -30,9 +26,11 @@ class PoissonScenario(ScenarioAlgorithm):
         while True:
             current_flight_level = int(self.flight_level())
             yield {
-                sg.START_TIME_KEY: np.random.exponential(scale=1/self.arrival_rate),
+                sg.START_TIME_KEY: random.expovariate(lambd = self.arrival_rate),
                 sg.CALLSIGN_KEY: next(self.callsign_generator()),
                 sg.AIRCRAFT_TYPE_KEY: self.aircraft_type(),
+                sg.DEPARTURE_KEY: '', # ScenarioGenerator.departure, # TODO.
+                sg.DESTINATION_KEY: '', # ScenarioGenerator.destination, # TODO.
                 sg.CURRENT_FLIGHT_LEVEL_KEY: current_flight_level,
                 sg.CLEARED_FLIGHT_LEVEL_KEY: current_flight_level,
                 sg.REQUESTED_FLIGHT_LEVEL_KEY: int(self.flight_level()),
@@ -40,24 +38,24 @@ class PoissonScenario(ScenarioAlgorithm):
             }
 
 
-    def aircraft(self, callsign, aircraft_type, start_time, current_flight_level,
-                 cleared_flight_level, requested_flight_level, route_index) -> dict:
-        """Returns all info required to create an aircraft, including its route."""
-
-        # Infer the start position from the route (i.e. take the first fix).
-        route = self.sector_element.shape.routes()[route_index]
-        start_position = route[0][0]
-
-        ret = {
-            CALLSIGN_KEY: callsign,
-            TYPE_KEY: aircraft_type,
-            DEPARTURE_KEY: ScenarioGenerator.departure,
-            DESTINATION_KEY: ScenarioGenerator.destination,
-            START_POSITION_KEY: start_position,
-            START_TIME_KEY: int(start_time),
-            CURRENT_FLIGHT_LEVEL_KEY: int(current_flight_level),
-            CLEARED_FLIGHT_LEVEL_KEY: int(cleared_flight_level),
-            REQUESTED_FLIGHT_LEVEL_KEY: int(requested_flight_level),
-            ROUTE_KEY: self.aircraft_route(route_index, level = requested_flight_level)
-        }
-        return ret
+    # def aircraft(self, callsign, aircraft_type, start_time, current_flight_level,
+    #              cleared_flight_level, requested_flight_level, route_index) -> dict:
+    #     """Returns all info required to create an aircraft, including its route."""
+    #
+    #     # Infer the start position from the route (i.e. take the first fix).
+    #     route = self.sector_element.shape.routes()[route_index]
+    #     start_position = route[0][0]
+    #
+    #     ret = {
+    #         CALLSIGN_KEY: callsign,
+    #         TYPE_KEY: aircraft_type,
+    #         DEPARTURE_KEY: ScenarioGenerator.departure,
+    #         DESTINATION_KEY: ScenarioGenerator.destination,
+    #         START_POSITION_KEY: start_position,
+    #         START_TIME_KEY: int(start_time),
+    #         CURRENT_FLIGHT_LEVEL_KEY: int(current_flight_level),
+    #         CLEARED_FLIGHT_LEVEL_KEY: int(cleared_flight_level),
+    #         REQUESTED_FLIGHT_LEVEL_KEY: int(requested_flight_level),
+    #         ROUTE_KEY: self.aircraft_route(route_index, level = requested_flight_level)
+    #     }
+    #     return ret
--- aviary/test/unit/scenario/test_poisson_scenario.py
+++ aviary/test/unit/scenario/test_poisson_scenario.py
@@ -2,171 +2,65 @@
 import pytest
 
 import aviary.scenario.poisson_scenario as ps
+import aviary.sector.sector_element as se
+import aviary.sector.sector_shape as ss
 import aviary.scenario.scenario_generator as sg
-# import aviary.sector.sector_shape as ss
-# import aviary.sector.sector_element as se
 
 @pytest.fixture(scope="function")
 def target():
     """Test fixture: a Poisson scenario object."""
 
     arrival_rate = 2 / 60 # Two arrivals per minute on average
-    return ps.PoissonScenario(arrival_rate = arrival_rate)
+    return ps.PoissonScenario(arrival_rate = arrival_rate,
+                              aircraft_types = ['B747', 'B777'],
+                              callsign_prefixes = ["SPEEDBIRD", "VJ", "DELTA", "EZY"],
+                              flight_levels = [200, 240, 280, 320, 360, 400],
+                              seed = 22)
 
 
-# def test_exponential_interarrival_times(target):
-#
-#     duration = 5 * 60 # Five minute scenario
-#     seed = 74
-#
-#     result = target.exponential_interarrival_times(duration = duration, seed = seed)
-#
-#     # With this seed, there are 11 arrivals.
-#     assert len(result) == 11
-#
-# def test_poisson_scenario(x_target):
-#
-#     arrival_rate = 2 / 60 # Two arrivals per minute on average
-#     duration = 5 * 60 # Five minute scenario
-#     seed = 74
-#     result = x_target.poisson_scenario(arrival_rate = arrival_rate, duration = duration, seed = seed)
-#
-#     assert isinstance(result, dict)
-#     assert 'startTime' in result
-#     assert 'aircraft' in result
-#
-#     assert isinstance(result['aircraft'], list)
-#
-#     # With this seed, there are 11 arrivals.
-#     assert len(result['aircraft']) == 11
+@pytest.fixture(scope="function")
+def i_element():
 
-#
-# def test_poisson_scenario(target):
-#
-#     duration = 5 * 60 # Five minute scenario
-#     seed = 74
-#     result = target.generate_scenario(duration = duration, seed = seed)
-#
-#     assert isinstance(result, dict)
-#     assert 'startTime' in result
-#     assert 'aircraft' in result
-#
-#     assert isinstance(result['aircraft'], list)
-#
-#     # With this seed, there are 11 arrivals.
-#     assert len(result['aircraft']) == 11
+    name = "EARTH"
+    origin = (51.5, -0.1275)
+    shape = ss.IShape(fix_names=['a', 'b', 'c', 'd', 'e'], route_names = ['up', 'down'])
+
+    lower_limit = 140
+    upper_limit = 400
+    return se.SectorElement(name, origin, shape, lower_limit, upper_limit)
 
 
+# Test the callsign generator method in the base class ScenarioAlgorithm.
+# This checks that the **kwargs are correctly passed to the superconstructor.
+def test_callsign_generator(target):
+
+    ctr = 0
+    for x in target.callsign_generator():
+        if ctr == 0:
+            assert x == 'EZY230'
+        if ctr == 1:
+            assert x == 'SPEEDBIRD215'
+        if ctr > 1:
+            break
+        ctr = ctr + 1
 
 
+def test_aircraft_generator(target, i_element):
 
-#old:
-# # Top to bottom route in X-sector is along fixes:
-# #   SIN (exterior top) -> GATES -> ABYSS (middle) -> HAUNT-> LIMBO (exterior bottom)
-# # Left to right route in X-sector is along fixes:
-# #   WITCH (exterior left) -> SIREN -> ABYSS (middle) -> DEMON -> SATAN (exterior right)
-#
-# def test_route_element(x_target):
-#
-#     fix_name = "SIN"
-#     level = 240
-#     result = x_target.route_element(fix_name = fix_name, level = level)
-#
-#     assert sorted(result.keys()) == sorted([
-#         sg.ROUTE_ELEMENT_NAME_KEY,
-#         sg.ROUTE_ELEMENT_TYPE_KEY,
-#         sg.ROUTE_ELEMENT_SPEED_KEY,
-#         sg.ROUTE_ELEMENT_LEVEL_KEY])
-#
-#     assert result[sg.ROUTE_ELEMENT_NAME_KEY] == fix_name
-#     assert result[sg.ROUTE_ELEMENT_TYPE_KEY] == se.FIX_VALUE
-#     assert result[sg.ROUTE_ELEMENT_SPEED_KEY] == 0
-#     assert result[sg.ROUTE_ELEMENT_LEVEL_KEY] == level
-#
-# def test_aircraft_route(x_target):
-#
-#     route_index = 0
-#     level = 240
-#     result = x_target.aircraft_route(route_index = route_index, level = level)
-#     assert isinstance(result, list)
-#
-#     assert len(result) == len(x_target.sector_element.shape.routes()[route_index])
-#
-#     # All ROUTE_ELEMENT_SPEED elements should be zero.
-#     for element_dict in result:
-#         assert element_dict[sg.ROUTE_ELEMENT_SPEED_KEY] == 0
-#
-#     # All ROUTE_ELEMENT_LEVEL elements should be zero, except the last two, which should be 240.
-#     for element_dict in result[:len(result) - 2]:
-#         assert element_dict[sg.ROUTE_ELEMENT_LEVEL_KEY] == 0
-#
-#     for element_dict in result[-2:]:
-#         assert element_dict[sg.ROUTE_ELEMENT_LEVEL_KEY] == level
-#
-#     # import pprint
-#     # pprint.pprint(result)
-#
-# def test_callsigns(x_target):
-#
-#     n = 3
-#     result = x_target.callsigns(n)
-#
-#     assert isinstance(result, list)
-#     assert len(result) == n
-#     assert all(isinstance(s, str) for s in result)
-#
-#
-# def test_aircraft(x_target):
-#
-#     callsign = "SPEEDBIRD770"
-#     aircraft_type = "B747"
-#
-#     route_index = 1
-#
-#     start_time = 0
-#     current_flight_level = 260
-#     cleared_flight_level = 260
-#     requested_flight_level = 350
-#
-#     result = x_target.aircraft(callsign = callsign, aircraft_type = aircraft_type, start_time = start_time,
-#                                current_flight_level = current_flight_level, cleared_flight_level = cleared_flight_level,
-#                                requested_flight_level = requested_flight_level, route_index = route_index)
-#
-#     assert isinstance(result, dict)
-#
-#     assert sg.CALLSIGN_KEY in result
-#     assert sg.ROUTE_KEY in result
-#     assert sg.START_TIME_KEY in result
-#     assert sg.START_POSITION_KEY in result
-#
-#     assert len(result[sg.ROUTE_KEY]) == len(x_target.sector_element.shape.routes()[route_index])
-#
-#     # Check that the start position matches the name of the first fix in the route.
-#     assert sg.ROUTE_ELEMENT_NAME_KEY in result[sg.ROUTE_KEY][0]
-#     assert result[sg.START_POSITION_KEY] == result[sg.ROUTE_KEY][0][sg.ROUTE_ELEMENT_NAME_KEY]
-#
-# def test_serialisation(x_target):
-#
-#     arrival_rate = 2 / 60 # Two arrivals per minute on average
-#     duration = 1 * 60 # One minute scenario
-#     seed = 74
-#     result = x_target.poisson_scenario(arrival_rate = arrival_rate, duration = duration, seed = seed)
-#
-#     serialised = json.dumps(result, sort_keys=True)
-#
-#     deserialised = json.loads(serialised)
-#
-#     assert isinstance(deserialised, dict)
-#     assert sorted(deserialised.keys()) == sorted([sg.AIRCRAFT_KEY, sg.START_TIME_KEY])
-#
-#     #print(serialised)
-#
-# # def test_write_json_scenario(x_target):
-# #     # Test JSON serialisation to file.
-# #
-# #     arrival_rate = 5 / 60 # Five arrivals per minute on average
-# #     duration = 10 * 60 # Ten minute scenario
-# #     seed = 74
-# #     scenario = x_target.poisson_scenario(arrival_rate = arrival_rate, duration = duration, seed = seed)
-# #
-# #     x_target.write_json_scenario(scenario = scenario, filename = "x_sector_hell_poisson_scenario")
+    ctr = 0
+    N = 200
+    interarrival_times = []
+    for x in target.aircraft_generator(i_element):
+
+        assert isinstance(x, dict)
+        assert sorted(x.keys()) == [sg.CALLSIGN_KEY, sg.CLEARED_FLIGHT_LEVEL_KEY, sg.CURRENT_FLIGHT_LEVEL_KEY,
+                                    sg.DEPARTURE_KEY, sg.DESTINATION_KEY, sg.REQUESTED_FLIGHT_LEVEL_KEY,
+                                    sg.ROUTE_KEY,sg.START_TIME_KEY, sg.AIRCRAFT_TYPE_KEY]
+        interarrival_times.append(x[sg.START_TIME_KEY])
+        if ctr > N:
+            break
+        ctr = ctr + 1
+
+    # Check the mean interarrival time against the arrival rate parameter (with a 5% tolerance).
+    mean = sum(interarrival_times) / N
+    assert mean == pytest.approx(1 / target.arrival_rate, rel = 0.05)
