55b5a6c43372c8a464214d79269873a7d6a49b3d
Tim Hobson <thobson@turing.ac.uk>
2019-11-15 16:24:57 +0000

    Implement aircraft_generator and add tests Tests currently fail as lookup tables are not yet plumbed-in.

--- aviary/scenario/overflier_climber_scenario.py
+++ aviary/scenario/overflier_climber_scenario.py
@@ -6,11 +6,9 @@ Scenario generation algorithm with a single overflier and a single climber.
 
 import random
 
-from geopy import Point
-from geopy.distance import distance, VincentyDistance
-
 from aviary.scenario.scenario_algorithm import ScenarioAlgorithm
 import aviary.scenario.scenario_generator as sg
+from aviary.geo.geo_helper import GeoHelper
 
 class OverflierClimberScenario(ScenarioAlgorithm):
     """An overflier-climber scenario generator for I, X, Y airspace sectors"""
@@ -25,12 +23,12 @@ class OverflierClimberScenario(ScenarioAlgorithm):
 
     # Overriding abstract method
     def aircraft_generator(self, sector) -> dict:
-        """Generates a sequence of aircraft constituting a scenario."""
+        """Generates a sequence of two aircraft whose default trajectories intersect at the centre of the sector."""
 
         # Construct the route.
-        route = self.route(sector)
-        overflier_departure = self.departure_airport(route)
-        overflier_destination = self.destination_airport(route)
+        overflier_route = self.route(sector)
+        overflier_departure = self.departure_airport(overflier_route)
+        overflier_destination = self.destination_airport(overflier_route)
 
         # Select the flight levels.
         overflier_flight_level = self.overflier_flight_level()
@@ -53,16 +51,20 @@ class OverflierClimberScenario(ScenarioAlgorithm):
                                                                                       upper_level=overflier_flight_level,
                                                                                       aircraft_type=climber_aircraft_type)
 
-        # Compute the initial positions of both aircraft.
-        # TODO FROM HERE.
-        # NOTE that this function VincentyDistance was bugfixed in geopy 1.13.0 but deprectaed in 1.14.0 (with no replacement?)
-        bearing = 0 # TODO: get the bearing from the centre waypoint to the next waypoint for the overflier
-        overflier_initial_lat_lon = VincentyDistance(miles=distMiles).destination(Point(lat1, lon1), bearing) # TODO.
+        # Location of the central fix of the sector (which is the conflict point).
+        lat1, lon1 = sector.centre_point()
 
+        # Compute the initial position of the overflier. Note lon/lat order!
+        o_lon2, o_lat2 = overflier_route[0][1].coords[0] # Initial waypoint position on the overflier's route
+        overflier_lat_lon = GeoHelper.waypoint_location(lat1, lon1, o_lat2, o_lon2, overflier_horizontal_distance)
+
+        # Truncate the route in light of the modified starting position.
+        overflier_truncated_route = self.truncate_route(overflier_route, overflier_lat_lon[0], overflier_lat_lon[1])
 
         # Construct the overflier.
         yield {
             sg.START_TIME_KEY: 0,
+            sg.START_POSITION_KEY: overflier_lat_lon,
             sg.CALLSIGN_KEY: next(self.callsign_generator()),
             sg.AIRCRAFT_TYPE_KEY: overflier_aircraft_type,
             sg.DEPARTURE_KEY: overflier_departure,
@@ -70,15 +72,24 @@ class OverflierClimberScenario(ScenarioAlgorithm):
             sg.CURRENT_FLIGHT_LEVEL_KEY: overflier_flight_level,
             sg.CLEARED_FLIGHT_LEVEL_KEY: overflier_flight_level,
             sg.REQUESTED_FLIGHT_LEVEL_KEY: overflier_flight_level,
-            sg.ROUTE_KEY: route
+            sg.ROUTE_KEY: overflier_truncated_route
         }
-
 
         # Construct the climber, which flies the reverse route, starting below the overflier and whose requested
         # flight level is greater than or equal to that of the overflier.
-        route.reverse()
+        climber_route = overflier_route.copy()
+        climber_route.reverse()
+
+        # Compute the initial position of the climber. Note lon/lat order!
+        c_lon2, c_lat2 = climber_route[0][1].coords[0] # Initial waypoint position on the climber's route
+        climber_lat_lon = GeoHelper.waypoint_location(lat1, lon1, c_lat2, c_lon2, climber_horizontal_distance)
+
+        # Truncate the route in light of the modified starting position.
+        climber_truncated_route = self.truncate_route(climber_route, climber_lat_lon[0], climber_lat_lon[1])
+
         yield {
             sg.START_TIME_KEY: 0,
+            sg.START_POSITION_KEY: climber_lat_lon,
             sg.CALLSIGN_KEY: next(self.callsign_generator()),
             sg.AIRCRAFT_TYPE_KEY: climber_aircraft_type,
             sg.DEPARTURE_KEY: overflier_destination, # Reversed overflier departure/destination.
@@ -86,9 +97,16 @@ class OverflierClimberScenario(ScenarioAlgorithm):
             sg.CURRENT_FLIGHT_LEVEL_KEY: climber_current_flight_level,
             sg.CLEARED_FLIGHT_LEVEL_KEY: climber_current_flight_level,
             sg.REQUESTED_FLIGHT_LEVEL_KEY: climber_requested_flight_level,
-            sg.ROUTE_KEY: route # Reversed overflier route.
+            sg.ROUTE_KEY: climber_truncated_route
         }
 
+    def truncate_route(self, route, initial_lat, initial_lon):
+        """Truncates a route in light of a given start position by removing fixes that are already passed."""
+
+        # Retain only those route elements that are closer to the final fix than the start_position.
+        final_lon, final_lat = route[-1][1].coords[0] # Note lon/lat order!
+        return [i for i in route if GeoHelper.distance(final_lat, final_lon, i[1].coords[0][1], i[1].coords[0][0]) <
+                GeoHelper.distance(final_lat, final_lon, initial_lat, initial_lon)]
 
     def overflier_flight_level(self):
         """Returns a random flight level, excluding the minimum level"""
--- aviary/test/unit/scenario/test_overflier_climber_scenario.py
+++ aviary/test/unit/scenario/test_overflier_climber_scenario.py
@@ -2,18 +2,18 @@
 import pytest
 
 import aviary.scenario.overflier_climber_scenario as ocs
-import aviary.sector.sector_element as se
-import aviary.sector.sector_shape as ss
 import aviary.scenario.scenario_generator as sg
+
+from aviary.geo.geo_helper import GeoHelper
 
 @pytest.fixture(scope="function")
 def target():
     """Test fixture: an overflier-climber scenario object."""
 
-    return ocs.OverflierClimberScenario(aircraft_types = ['B747', 'B777'],
-                              callsign_prefixes = ["SPEEDBIRD", "VJ", "DELTA", "EZY"],
-                              flight_levels = [200, 240, 280, 320, 360, 400],
-                              seed = 22)
+    return ocs.OverflierClimberScenario(aircraft_types = ['B744', 'B743'],
+                                        callsign_prefixes = ["SPEEDBIRD", "VJ", "DELTA", "EZY"],
+                                        flight_levels = [200, 400],
+                                        seed = 223)
 
 
 # Test the callsign generator method in the base class ScenarioAlgorithm.
@@ -23,39 +23,12 @@ def test_callsign_generator(target):
     ctr = 0
     for x in target.callsign_generator():
         if ctr == 0:
-            assert x == 'EZY230'
+            assert x == 'VJ597'
         if ctr == 1:
-            assert x == 'SPEEDBIRD215'
+            assert x == 'SPEEDBIRD681'
         if ctr > 1:
             break
         ctr = ctr + 1
-
-
-def test_aircraft_generator(target, i_element):
-
-    # Test across multiple generated scenarios.
-    for i in range(0, 10):
-
-        ctr = 0
-        for x in target.aircraft_generator(i_element):
-
-            assert isinstance(x, dict)
-            assert sorted(x.keys()) == [sg.CALLSIGN_KEY, sg.CLEARED_FLIGHT_LEVEL_KEY, sg.CURRENT_FLIGHT_LEVEL_KEY,
-                                        sg.DEPARTURE_KEY, sg.DESTINATION_KEY, sg.REQUESTED_FLIGHT_LEVEL_KEY,
-                                        sg.ROUTE_KEY,sg.START_TIME_KEY, sg.AIRCRAFT_TYPE_KEY]
-
-            print(x)
-            ctr = ctr + 1
-            if ctr == 1:
-                overflier = x
-            if ctr == 2:
-                climber = x
-
-        # Check that the scenario contains precisely two aircraft.
-        assert ctr == 2
-
-        assert overflier[sg.CURRENT_FLIGHT_LEVEL_KEY] > climber[sg.CURRENT_FLIGHT_LEVEL_KEY]
-        assert overflier[sg.CURRENT_FLIGHT_LEVEL_KEY] <= climber[sg.REQUESTED_FLIGHT_LEVEL_KEY]
 
 
 def test_overflier_flight_level(target):
@@ -64,3 +37,64 @@ def test_overflier_flight_level(target):
         result = target.overflier_flight_level()
         assert result in target.flight_levels
         assert result > min(target.flight_levels)
+
+
+def test_aircraft_generator(target, i_element):
+
+    # Test across multiple generated scenarios.
+    ctr = 0
+    for x in target.aircraft_generator(i_element):
+
+        assert isinstance(x, dict)
+        assert sorted(x.keys()) == [sg.CALLSIGN_KEY, sg.CLEARED_FLIGHT_LEVEL_KEY, sg.CURRENT_FLIGHT_LEVEL_KEY,
+                                    sg.DEPARTURE_KEY, sg.DESTINATION_KEY, sg.REQUESTED_FLIGHT_LEVEL_KEY, sg.ROUTE_KEY,
+                                    sg.START_POSITION_KEY, sg.START_TIME_KEY, sg.AIRCRAFT_TYPE_KEY]
+
+        ctr = ctr + 1
+        if ctr == 1:
+            overflier = x
+        if ctr == 2:
+            climber = x
+
+    # Check that the scenario contains precisely two aircraft.
+    assert ctr == 2
+
+    assert overflier[sg.AIRCRAFT_TYPE_KEY] == 'B744'
+    assert climber[sg.AIRCRAFT_TYPE_KEY] == 'B743'
+
+    assert overflier[sg.CURRENT_FLIGHT_LEVEL_KEY] == 400
+    assert climber[sg.CURRENT_FLIGHT_LEVEL_KEY] == 200
+    assert climber[sg.REQUESTED_FLIGHT_LEVEL_KEY] == 400
+
+    # TODO: include these:
+    # assert [i[0] for i in overflier[sg.ROUTE_KEY]] == ['C', 'B', 'A']
+    # assert [i[0] for i in climber[sg.ROUTE_KEY]] == ['C', 'D', 'E']
+
+    # Check that the aircraft will be in conflict at the centre point of the sector (assuming default trajectories).
+
+    overflier_speed = 316.7942522 # From the lookup tables (m/s)
+    lat1, lon1 = overflier[sg.START_POSITION_KEY]
+    lat2, lon2 = i_element.centre_point()
+    overflier_distance = GeoHelper.distance(lat1 = lat1, lon1 = lon1, lat2 = lat2, lon2 = lon2)
+    time_to_conflict = overflier_distance/overflier_speed
+
+    climber_time_to_conflict_level = 2016 - 492
+
+    # We expect the time taken for the overflier to reach the conflict location to be
+    # (approximately) equal to the time taken for the climber to ascend to the overflier's level.
+    assert time_to_conflict == pytest.approx(climber_time_to_conflict_level)
+
+    # We also expect the climber to have reached the conflict location at the same time.
+    lat1, lon1 = climber[sg.START_POSITION_KEY]
+    lat2, lon2 = i_element.centre_point()
+    climber_distance = GeoHelper.distance(lat1 = lat1, lon1 = lon1, lat2 = lat2, lon2 = lon2)
+
+    downtrack_distance = 389237.4376 - 71745.47755 # From the lookup tables (m)
+
+    assert downtrack_distance == pytest.approx(climber_distance)
+
+    print("overflier:")
+    print(overflier)
+
+    print("climber:")
+    print(climber)
