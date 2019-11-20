4f84d12426edec27f0aa63668edf25eb33df8f5e
Tim Hobson <thobson@turing.ac.uk>
2019-11-20 11:49:41 +0000

    Fix bug in waypoint location calculation

--- aviary/geo/geo_helper.py
+++ aviary/geo/geo_helper.py
@@ -12,13 +12,14 @@ class GeoHelper():
     @staticmethod
     def waypoint_location(lat1, lon1, lat2, lon2, distance_m, geod = Geodesic.WGS84):
         """
-        Computes the location of waypoint at a given distance (in metres) between two points.
+        Computes the location of waypoint at a given distance (in metres) from
+        point (lat1, lon1) in the direction of (lat2, lon2).
+
         Based on the example at https://geographiclib.sourceforge.io/html/python/examples.html#computing-waypoints
         """
 
         l = geod.InverseLine(lat1, lon1, lat2, lon2)
-        s = min(distance_m, l.s13)
-        g = l.Position(s, Geodesic.STANDARD)
+        g = l.Position(distance_m, Geodesic.STANDARD)
         return g['lat2'], g['lon2']
 
 
@@ -26,6 +27,7 @@ class GeoHelper():
     def distance(lat1, lon1, lat2, lon2, geod = Geodesic.WGS84):
         """
         Computes the distance in metres between two geographic points.
+
         Based on the example at https://geographiclib.sourceforge.io/html/python/examples.html#basic-geodesic-calculations
         """
 
--- aviary/test/unit/geo/test_geo_helper.py
+++ aviary/test/unit/geo/test_geo_helper.py
@@ -3,6 +3,15 @@ import pytest
 
 from aviary.geo.geo_helper import GeoHelper
 
+
+def test_distance():
+
+    lat1, lon1 = 51.5080, -0.1281
+    lat2, lon2 = 50.6083, -1.9608
+
+    result = GeoHelper.distance(lat1, lon1, lat2, lon2)
+    assert result == pytest.approx(162.5e3, 0.01)
+
 def test_waypoint_location():
 
     lat1, lon1 = 51.5080, -0.1281
@@ -16,5 +25,9 @@ def test_waypoint_location():
 
     result = GeoHelper.waypoint_location(lat1, lon1, lat2, lon2, distance_m = 81.25e3)
     assert result == pytest.approx((51.06276, -1.051272), 0.01)
+
+    # Check the behaviour in case the distance exceeds the separation between the fixes.
+    result = GeoHelper.waypoint_location(lat1, lon1, lat2, lon2, distance_m = 250e3)
+    assert result == pytest.approx((50.1154, -2.9125), 0.01)
 
 
