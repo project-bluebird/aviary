e00b0e94099089ce081495f3839604a5b0656cd2
Radka Jersakova <r.jersakova@gmail.com>
2019-11-22 10:23:16 +0000

    Update sector definition to new GeoJSON format

--- aviary/sector/sector_element.py
+++ aviary/sector/sector_element.py
@@ -36,13 +36,17 @@ LONGITUDE_KEY = "longitude"
 LATITUDES_KEY = "latitudes"
 LONGITUDES_KEY = "longitudes"
 POINTS_KEY = "points"
+CHILDREN_KEY = "children"
+CHILDREN_NAMES_KEY = "names"
 
 # JSON values
 FEATURE_VALUE = "Feature"
 SECTOR_VALUE = "SECTOR"
 FIX_VALUE = "FIX"
 ROUTE_VALUE = "ROUTE"
+SECTOR_VOLUME_VALUE = "SECTOR_VOLUME"
 POLYGON_VALUE = "Polygon"
+POINT_VALUE = "Point"
 
 class SectorElement():
     """An elemental sector of airspace"""
@@ -86,7 +90,6 @@ class SectorElement():
         shape_routes = self.shape.routes()
         return [[(i[0], self.__inv_project__(i[1])) for i in route] for route in shape_routes]
 
-
     @staticmethod
     def truncate_route(route, initial_lat, initial_lon):
         """Truncates a route in light of a given start position by removing fixes that are already passed."""
@@ -105,48 +108,103 @@ class SectorElement():
 
         # Build the list of features: one for the boundary, one for each fix and one for each route.
         geojson = {FEATURES_KEY: []}
+        geojson[FEATURES_KEY].append(self.sector_geojson())
         geojson[FEATURES_KEY].append(self.boundary_geojson())
         geojson[FEATURES_KEY].extend([self.route_geojson(route_index) for route_index in range(0, len(self.shape.route_names))])
         geojson[FEATURES_KEY].extend([self.waypoint_geojson(name) for name in self.shape.fixes.keys()])
 
         return geojson
 
-    def boundary_geojson(self) -> dict:
-        """Return a GeoJSON dictionary representing the sector boundary"""
+    def hash_sector_coordinates(self) -> str:
+        """Returns hash of the sector boundary coordinates as string"""
+        return str(hash(self.__inv_project__(self.shape.polygon).__geo_interface__[COORDINATES_KEY][0]))
 
-        # Note: lower & upper limits are serialised in a list. TODO: is there a reason for wrapping in a list?.
+    def sector_geojson(self) -> dict:
+        """
+        Return a GeoJSON dictionary representing the sector. The sector has an
+        associated sector volume (child property) that describes the sector boundaries.
+        The ID of the sector volume is a hash of the sector coordinates.
+
+        A sector includes elements:
+        - type: "Feature"
+        - geometry: {}
+        - properties:
+            - name: e.g. "HELL"
+            - type: "SECTOR"
+            - children: {
+                "SECTOR_VOLUME": {"names": [<hash of sector boundary coordinates>]},
+                "ROUTE":{"names": [<shape route names>]}
+                }
+        """
+
         geojson = {
             TYPE_KEY: FEATURE_VALUE,
             PROPERTIES_KEY: {
                 NAME_KEY: self.name,
                 TYPE_KEY: SECTOR_VALUE,
-                LOWER_LIMIT_KEY: [self.lower_limit],
-                UPPER_LIMIT_KEY: [self.upper_limit],
-                ROUTES_KEY: self.shape.named_routes()
+                CHILDREN_KEY: {
+                    SECTOR_VOLUME_VALUE : {CHILDREN_NAMES_KEY: [self.hash_sector_coordinates()]},
+                    ROUTE_VALUE: {CHILDREN_NAMES_KEY: self.shape.route_names}
+                }
             },
-            GEOMETRY_KEY: self.__inv_project__(self.shape.polygon).__geo_interface__
+            GEOMETRY_KEY: {}
         }
-
-        # Fix issue with __geo_interface__ unexpectedly returning a tuple of coordinates rather than a list.
-        geojson = self.fix_geometry_coordinates_tuple(geojson)
 
         return geojson
 
-    def waypoint_geojson(self, name) -> dict:
-        """Return a GeoJSON dictionary representing the waypoints"""
+    def boundary_geojson(self) -> dict:
+        """
+        Return a GeoJSON dictionary representing the sector boundary (volume).
 
-        latitude = self.__inv_project__(self.shape.fixes[name]).coords[0][1]
-        longitude = self.__inv_project__(self.shape.fixes[name]).coords[0][0]
+        A sector volume includes elements:
+        - type: "Feature"
+        - geometry: A Polygon feature whore properties are a list of long/lat coordinates defining the sector boundaries
+        - properties:
+            - name: ID derived from hashing the volume's coordinates
+            - type: "SECTOR_VOLUME"
+            - lower_limit: e.g. 150
+            - upper_limit: e.g. 400
+            - children: {}
+        """
+
+        geojson = {
+            TYPE_KEY : FEATURE_VALUE,
+            GEOMETRY_KEY : self.__inv_project__(self.shape.polygon).__geo_interface__,
+            PROPERTIES_KEY : {
+                NAME_KEY: self.hash_sector_coordinates(),
+                TYPE_KEY: SECTOR_VOLUME_VALUE,
+                LOWER_LIMIT_KEY: self.lower_limit,
+                UPPER_LIMIT_KEY: self.upper_limit,
+                CHILDREN_KEY: {}
+            }
+        }
+
+        geojson = self.fix_geometry_coordinates_tuple(geojson)
+        return geojson
+
+
+    def waypoint_geojson(self, name) -> dict:
+        """
+        Return a GeoJSON dictionary representing the waypoints (fixes)
+
+        A waypoint includes elements:
+        - type: "Feature"
+        - geometry: a Point feature with a list of long/lat coordinates
+        - properties:
+            - name: e.g. "LIMBO"
+            - type: "FIX"
+        """
+
         geojson = {
             TYPE_KEY: FEATURE_VALUE,
             PROPERTIES_KEY: {
                 NAME_KEY: name.upper(),
-                LATITUDE_KEY: latitude,
-                LONGITUDE_KEY: longitude,
                 TYPE_KEY: FIX_VALUE
             },
             GEOMETRY_KEY: self.__inv_project__(self.shape.fixes[name]).__geo_interface__
         }
+
+        geojson = self.fix_geometry_coordinates_tuple(geojson)
         return geojson
 
     def route_geojson(self, route_index) -> dict:
@@ -157,28 +215,25 @@ class SectorElement():
 
         A route includes elements:
         - type: "Feature"
-        - geometry: a LineString feature whose 'properties' are a list of points, with long/lat repeating those above, plus altitudes
+        - geometry: a LineString feature whose properties are a list of points, with long/lat coordinates
         - properties:
-           - points: list of fix names
-           - latitudes:
-           - longitudes:
-           - altitudes:
            - name: e.g. "DAMNATION"
            - type: "ROUTE"
+           - children: {"FIX": {"names": [<route fix names>]}}
         """
 
         route = self.shape.routes()[route_index]
         fix_names = [fix_item[0] for fix_item in route]
-        latitudes = [self.__inv_project__(fix_item[1]).coords[0][1] for fix_item in route]
-        longitudes = [self.__inv_project__(fix_item[1]).coords[0][0] for fix_item in route]
         geojson = {
             TYPE_KEY: FEATURE_VALUE,
             PROPERTIES_KEY: {
-                POINTS_KEY: fix_names,
-                LATITUDES_KEY: latitudes,
-                LONGITUDES_KEY: longitudes,
                 NAME_KEY: self.shape.route_names[route_index],
-                TYPE_KEY: ROUTE_VALUE
+                TYPE_KEY: ROUTE_VALUE,
+                CHILDREN_KEY: {
+                    FIX_VALUE: {
+                        CHILDREN_NAMES_KEY: fix_names
+                    }
+                }
             },
             GEOMETRY_KEY: self.__inv_project__(LineString([fix_item[1] for fix_item in route])).__geo_interface__
         }
--- aviary/test/unit/sector/test_sector_element.py
+++ aviary/test/unit/sector/test_sector_element.py
@@ -1,13 +1,13 @@
 
 import pytest
 
+import os
 import geojson
 
 import aviary.sector.sector_element as se
 
-
 def test_centre_point(i_element):
-
+    
     result = i_element.centre_point()
     assert result == pytest.approx((51.5, -0.1275), 0.0001)
 
@@ -57,27 +57,75 @@ def test_truncate_route(i_element):
     assert i_element.truncate_route(route, initial_lat = (latB + latC)/2, initial_lon = lonA) == route[2:]
 
 
+def test_sector_geojson(i_element):
+
+    result = i_element.sector_geojson()
+
+    assert sorted(result.keys()) == sorted([se.GEOMETRY_KEY, se.PROPERTIES_KEY, se.TYPE_KEY])
+
+    assert result[se.TYPE_KEY] == se.FEATURE_VALUE
+
+    assert isinstance(result[se.PROPERTIES_KEY], dict)
+    assert sorted(result[se.PROPERTIES_KEY].keys()) == \
+           sorted([se.CHILDREN_KEY, se.NAME_KEY, se.TYPE_KEY])
+
+    assert result[se.PROPERTIES_KEY][se.TYPE_KEY] == se.SECTOR_VALUE
+
+    assert isinstance(result[se.PROPERTIES_KEY][se.CHILDREN_KEY], dict)
+    assert sorted(result[se.PROPERTIES_KEY][se.CHILDREN_KEY].keys()) == \
+           sorted([se.ROUTE_VALUE, se.SECTOR_VOLUME_VALUE])
+
+    assert isinstance(result[se.PROPERTIES_KEY][se.CHILDREN_KEY][se.ROUTE_VALUE], dict)
+    assert sorted(result[se.PROPERTIES_KEY][se.CHILDREN_KEY][se.ROUTE_VALUE].keys()) == \
+           [se.CHILDREN_NAMES_KEY]
+
+    assert isinstance(result[se.PROPERTIES_KEY][se.CHILDREN_KEY][se.ROUTE_VALUE][se.CHILDREN_NAMES_KEY], list)
+    assert len(result[se.PROPERTIES_KEY][se.CHILDREN_KEY][se.ROUTE_VALUE][se.CHILDREN_NAMES_KEY]) == len(i_element.shape.route_names)
+
+    assert isinstance(result[se.PROPERTIES_KEY][se.CHILDREN_KEY][se.SECTOR_VOLUME_VALUE], dict)
+    assert sorted(result[se.PROPERTIES_KEY][se.CHILDREN_KEY][se.SECTOR_VOLUME_VALUE].keys()) == [se.CHILDREN_NAMES_KEY]
+    assert len(result[se.PROPERTIES_KEY][se.CHILDREN_KEY][se.SECTOR_VOLUME_VALUE]) == 1
+
+    assert isinstance(result[se.PROPERTIES_KEY][se.CHILDREN_KEY][se.SECTOR_VOLUME_VALUE][se.CHILDREN_NAMES_KEY], list)
+
 def test_boundary_geojson(i_element):
 
     result = i_element.boundary_geojson()
 
-    assert sorted(result.keys()) == [se.GEOMETRY_KEY, se.PROPERTIES_KEY, se.TYPE_KEY]
-    assert result[se.GEOMETRY_KEY][se.TYPE_KEY] == 'Polygon'
+    assert sorted(result.keys()) == sorted([se.GEOMETRY_KEY, se.PROPERTIES_KEY, se.TYPE_KEY])
 
-    assert isinstance(result[se.PROPERTIES_KEY], dict)
+    assert result[se.TYPE_KEY] == se.FEATURE_VALUE
+
+    assert sorted(result[se.GEOMETRY_KEY].keys()) == sorted([se.COORDINATES_KEY, se.TYPE_KEY])
+
+    assert result[se.GEOMETRY_KEY][se.TYPE_KEY] == se.POLYGON_VALUE
+    assert isinstance(result[se.GEOMETRY_KEY][se.COORDINATES_KEY], list)
+    # TODO: check length of coordinates
+
     assert sorted(result[se.PROPERTIES_KEY].keys()) == \
-           sorted([se.NAME_KEY, se.LOWER_LIMIT_KEY, se.ROUTES_KEY, se.TYPE_KEY, se.UPPER_LIMIT_KEY])
+           sorted([se.NAME_KEY, se.TYPE_KEY, se.UPPER_LIMIT_KEY, se.LOWER_LIMIT_KEY, se.CHILDREN_KEY])
 
-    assert isinstance(result[se.PROPERTIES_KEY][se.ROUTES_KEY], dict)
-    assert sorted(result[se.PROPERTIES_KEY][se.ROUTES_KEY].keys()) == sorted(i_element.shape.route_names)
+    assert isinstance(result[se.PROPERTIES_KEY][se.NAME_KEY], str)
+    assert result[se.PROPERTIES_KEY][se.TYPE_KEY] == se.SECTOR_VOLUME_VALUE
+    assert isinstance(result[se.PROPERTIES_KEY][se.UPPER_LIMIT_KEY], int)
+    assert isinstance(result[se.PROPERTIES_KEY][se.LOWER_LIMIT_KEY], int)
+    assert isinstance(result[se.PROPERTIES_KEY][se.CHILDREN_KEY], dict)
 
 def test_waypoint_geojson(i_element):
 
     name = 'b'.upper()
     result = i_element.waypoint_geojson(name)
 
-    assert sorted(result.keys()) == [se.GEOMETRY_KEY, se.PROPERTIES_KEY, se.TYPE_KEY]
-    assert result[se.GEOMETRY_KEY][se.TYPE_KEY] == 'Point'
+    assert sorted(result.keys()) == sorted([se.GEOMETRY_KEY, se.PROPERTIES_KEY, se.TYPE_KEY])
+
+    assert result[se.TYPE_KEY] == se.FEATURE_VALUE
+
+    assert sorted(result[se.GEOMETRY_KEY].keys()) == sorted([se.COORDINATES_KEY, se.TYPE_KEY])
+    assert result[se.GEOMETRY_KEY][se.TYPE_KEY] == se.POINT_VALUE
+    assert isinstance(result[se.GEOMETRY_KEY][se.COORDINATES_KEY], list)
+    assert len(result[se.GEOMETRY_KEY][se.COORDINATES_KEY]) == 2
+
+    assert sorted(result[se.PROPERTIES_KEY].keys()) == sorted([se.NAME_KEY, se.TYPE_KEY])
     assert result[se.PROPERTIES_KEY][se.NAME_KEY] == name.upper()
     assert result[se.PROPERTIES_KEY][se.TYPE_KEY] == se.FIX_VALUE
 
@@ -86,19 +134,24 @@ def test_route_geojson(i_element):
     route_index = 1
     result = i_element.route_geojson(route_index = route_index)
 
-    assert sorted(result.keys()) == [se.GEOMETRY_KEY, se.PROPERTIES_KEY, se.TYPE_KEY]
-    assert sorted(result[se.PROPERTIES_KEY]) == [se.LATITUDES_KEY, se.LONGITUDES_KEY, se.NAME_KEY, se.POINTS_KEY, se.TYPE_KEY]
-    assert len(result[se.PROPERTIES_KEY][se.LATITUDES_KEY]) == 5
-    assert len(result[se.PROPERTIES_KEY][se.LONGITUDES_KEY]) == 5
+    assert sorted(result.keys()) == sorted([se.GEOMETRY_KEY, se.PROPERTIES_KEY, se.TYPE_KEY])
+
+    assert result[se.TYPE_KEY] == se.FEATURE_VALUE
+
+    assert sorted(result[se.PROPERTIES_KEY]) == \
+           sorted([se.CHILDREN_KEY, se.NAME_KEY, se.TYPE_KEY])
+
     assert result[se.PROPERTIES_KEY][se.NAME_KEY] == i_element.shape.route_names[route_index]
-    assert len(result[se.PROPERTIES_KEY][se.POINTS_KEY]) == 5
     assert result[se.PROPERTIES_KEY][se.TYPE_KEY] == se.ROUTE_VALUE
+    assert sorted(result[se.PROPERTIES_KEY][se.CHILDREN_KEY].keys()) == [se.FIX_VALUE]
+    assert isinstance(result[se.PROPERTIES_KEY][se.CHILDREN_KEY][se.FIX_VALUE][se.CHILDREN_NAMES_KEY], list)
+    assert len(result[se.PROPERTIES_KEY][se.CHILDREN_KEY][se.FIX_VALUE][se.CHILDREN_NAMES_KEY]) == len(i_element.shape.fixes)
 
     assert isinstance(result[se.GEOMETRY_KEY], dict)
-    assert sorted(result[se.GEOMETRY_KEY].keys()) == ['coordinates', se.TYPE_KEY]
+    assert sorted(result[se.GEOMETRY_KEY].keys()) == sorted([se.COORDINATES_KEY, se.TYPE_KEY])
 
-    assert isinstance(result[se.GEOMETRY_KEY]['coordinates'], list)
-    assert len(result[se.GEOMETRY_KEY]['coordinates']) == len(result[se.PROPERTIES_KEY][se.POINTS_KEY])
+    assert isinstance(result[se.GEOMETRY_KEY][se.COORDINATES_KEY], list)
+    assert len(result[se.GEOMETRY_KEY][se.COORDINATES_KEY]) == len(i_element.shape.fixes)
 
 
 def test_geo_interface(y_element):
@@ -107,8 +160,8 @@ def test_geo_interface(y_element):
 
     assert sorted(result.keys()) == [se.FEATURES_KEY]
 
-    # The result contains one feature per route and per waypoint, plus one for the boundary.
-    assert len(result[se.FEATURES_KEY]) == len(y_element.shape.route_names) + len(y_element.shape.fixes) + 1
+    # The result contains one feature per route and per waypoint, plus one for the sector and one for the sector boundary/volume.
+    assert len(result[se.FEATURES_KEY]) == len(y_element.shape.route_names) + len(y_element.shape.fixes) + 2
 
 
 def test_serialisation(x_element):
@@ -122,7 +175,16 @@ def test_serialisation(x_element):
     assert list(deserialised.keys()) == [se.FEATURES_KEY]
 
 
-# def test_write_geojson(x_element):
-#     # Test JSON serialisation/deserialisation.
-#
-#     x_element.write_geojson(filename = "x_sector_hell")
+def test_write_geojson(x_element):
+
+    filename = "x_sector_hell"
+    here = os.path.abspath(os.path.dirname(__file__))
+    file = x_element.write_geojson(
+        filename = filename,
+        path = here
+        )
+
+    assert os.path.exists(file)
+
+    # Clean up.
+    os.remove(file)
