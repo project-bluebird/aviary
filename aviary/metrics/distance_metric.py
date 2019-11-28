6f1814a55bc789b577c83ce6314303cb616cbb81
radka-j <r.jersakova@hotmail.com>
2019-11-28 22:45:05 +0000

    refactor metrics

--- aviary/metrics/distance_metric.py
+++ aviary/metrics/distance_metric.py
@@ -1,16 +1,5 @@
 """
 Abstract base class representing a distance based metric.
-
-denote:
-- x = (x_lat, x_lon, x_alt) := location of point x
-- y = (y_lat, y_lon, y_alt) := location of point y
-- d_h = Geodesic distance between(x_lat, x_lon) and (y_lat, y_lon)
-- d_v = | x_alt - y_alt |, the absolute vertical distance between x & y
-
-A general class of metrics takes the form:
-- m(d_h, d_v) = max{ m_h(d_h), m_v(d_v) }
-
-Where m_h and m_v are functions depending on horizontal & vertical (absolute) distance, respectively.
 """
 
 from pyproj import Geod
@@ -26,27 +15,31 @@ _ONE_NM = 1852  # Meters
 class DistanceMetric(ABC):
 
     @abstractmethod
-    def score(self) -> float:
+    def score():
         pass
 
-    def horizontal_distance_m(self, lon1, lat1, lon2, lat2):
+    @staticmethod
+    def horizontal_distance_m(lon1, lat1, lon2, lat2):
         """Horizontal distance between two (lon/lat) points in metres"""
 
         _, _, horizontal_m = _WGS84.inv(lon1, lat1, lon2, lat2)
         return horizontal_m
 
-    def horizontal_distance_nm(self, lon1, lat1, lon2, lat2):
+    @staticmethod
+    def horizontal_distance_nm(lon1, lat1, lon2, lat2):
         """Horizontal distance between two (lon/lat) points in nautical miles"""
 
-        horizontal_m = self.horizontal_distance_m(lon1, lat1, lon2, lat2)
+        horizontal_m = DistanceMetric.horizontal_distance_m(lon1, lat1, lon2, lat2)
         return round(horizontal_m / _ONE_NM)
 
-    def vertical_distance_m(self, alt1, alt2):
+    @staticmethod
+    def vertical_distance_m(alt1, alt2):
         """Vertical distance between two altitudes in metres"""
 
         return abs(alt1 - alt2)
 
-    def vertical_distance_ft(self, alt1, alt2):
+    @staticmethod
+    def vertical_distance_ft(alt1, alt2):
         """Vertical distance in feet between two altitudes in metres"""
 
-        return self.vertical_distance_m(alt1, alt2) * _SCALE_METRES_TO_FEET
+        return DistanceMetric.vertical_distance_m(alt1, alt2) * _SCALE_METRES_TO_FEET
--- aviary/test/unit/metrics/test_separation_metric.py
+++ aviary/test/unit/metrics/test_separation_metric.py
@@ -3,6 +3,7 @@ import pytest
 
 from aviary.metrics.separation_metric import AircraftSeparation
 
+_SCALE_METRES_TO_FEET = 3.280839895
 
 def test_separation_score():
 
@@ -17,13 +18,22 @@ def test_separation_score():
 
     assert metric.score(lon1, lat1, alt1, lon2, lat2, alt2) == 0
 
-    lon2, lat2, alt2 = (0, 0, 1000)
+    lon2, lat2, alt2 = (0, 5, 0)
 
     assert metric.score(lon1, lat1, alt1, lon2, lat2, alt2) == 0
 
-    lon1, lat1, alt1 = (0, 5, 0)
-    lon2, lat2, alt2 = (0, 0, 0)
+    lon2, lat2, alt2 = (0, 0, 1000/_SCALE_METRES_TO_FEET)
+
+    assert metric.score(lon1, lat1, alt1, lon2, lat2, alt2) == -1
+
+    lon2, lat2, alt2 = (0, 0, 1230/_SCALE_METRES_TO_FEET)
+
+    assert metric.score(lon1, lat1, alt1, lon2, lat2, alt2) == -0.77
+
+    lon2, lat2, alt2 = (0, 0, 1500/_SCALE_METRES_TO_FEET)
+
+    assert metric.score(lon1, lat1, alt1, lon2, lat2, alt2) == -0.5
+
+    lon2, lat2, alt2 = (0, 0, 2000/_SCALE_METRES_TO_FEET)
 
     assert metric.score(lon1, lat1, alt1, lon2, lat2, alt2) == 0
-
-    
