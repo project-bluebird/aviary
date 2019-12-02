7f00599b8a8a1ba797cdadc87e2cb578fda526ac
Radka Jersakova <r.jersakova@gmail.com>
2019-12-02 16:54:48 +0000

    add and update tests

--- aviary/test/unit/metrics/test_utils.py
+++ aviary/test/unit/metrics/test_utils.py
@@ -18,13 +18,3 @@ def test_horizontal_distance():
 
     result_nm = utils.horizontal_distance_nm(lon1, lat1, lon2, lat2)
     assert result_nm == pytest.approx(round(result/utils._ONE_NM))
-
-
-def test_vertical_distance():
-
-    alt = 123456789.123456789
-    assert utils.vertical_distance_m(alt, alt) == 0
-    assert utils.vertical_distance_ft(alt, alt) == 0
-
-    assert utils.vertical_distance_m(300, 150) == 150
-    assert utils.vertical_distance_ft(300, 150) == 150 * utils._SCALE_METRES_TO_FEET
