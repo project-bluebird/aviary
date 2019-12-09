a0de8a55442488af645a794a7737d502ace1ea36
Tim Hobson <thobson@turing.ac.uk>
2019-12-09 10:00:38 +0000

    Add abstract base class and global tp variable

--- aviary/test/unit/trajectory/test_lookup_trajectory_predictor.py
+++ aviary/test/unit/trajectory/test_lookup_trajectory_predictor.py
@@ -1,16 +1,17 @@
 
 import pytest
 
-import aviary.trajectory.lookup_trajectory_predictor as tp
+from aviary.trajectory.lookup_trajectory_predictor import LookupTrajectoryPredictor
+import aviary.trajectory.trajectory_predictor as tp
 
 
 @pytest.fixture(scope="function")
 def target(cruise_speed_dataframe, climb_time_dataframe, downtrack_distance_dataframe):
     """Test fixture: a TrajectoryPredictor object"""
 
-    return tp.LookupTrajectoryPredictor(cruise_speed_lookup = cruise_speed_dataframe,
-                                  climb_time_lookup = climb_time_dataframe,
-                                  downtrack_distance_lookup = downtrack_distance_dataframe)
+    return LookupTrajectoryPredictor(cruise_speed_lookup = cruise_speed_dataframe,
+                                     climb_time_lookup = climb_time_dataframe,
+                                     downtrack_distance_lookup = downtrack_distance_dataframe)
 
 
 def test_cruise_speed(target):
@@ -24,6 +25,7 @@ def test_cruise_speed(target):
     with pytest.raises(Exception):
         target.cruise_speed(flight_level = 200, aircraft_type = 'X744')
 
+
 def test_climb_time_to_level(target):
 
     assert target.climb_time_to_level(flight_level = 200, aircraft_type = 'B743') == 500
@@ -34,4 +36,17 @@ def test_downtrack_distance_to_level(target):
 
     assert target.downtrack_distance_to_level(flight_level = 360, aircraft_type = 'B743') == 220000.7294
     assert target.downtrack_distance_to_level(flight_level = 400, aircraft_type = 'B744') == 350000.8483
+
+
+def test_load_trajectory_lookups(cruise_speed_dataframe, climb_time_dataframe, downtrack_distance_dataframe):
+
+    assert tp.global_trajectory_predictor is None
+    LookupTrajectoryPredictor.load_trajectory_lookups(cruise_speed_lookup=cruise_speed_dataframe,
+                                                      climb_time_lookup=climb_time_dataframe,
+                                                      downtrack_distance_lookup=downtrack_distance_dataframe)
+
+    assert tp.global_trajectory_predictor is not None
+    assert tp.global_trajectory_predictor.cruise_speed(flight_level = 200, aircraft_type = 'B744') == 210.7354179
+    assert tp.global_trajectory_predictor.climb_time_to_level(flight_level = 200, aircraft_type = 'B743') == 500
+    assert tp.global_trajectory_predictor.downtrack_distance_to_level(flight_level = 400, aircraft_type = 'B744') == 350000.8483
 
