project-bluebird/aviary

commit 8f6bdbce40a0a317d2495f4809f81342de9bd676

Fix invalid sector GeoJSON
coordinates should be in a nested list

aviary/parser/bluesky_parser.py
@@ -131,7 +131,7 @@ class BlueskyParser(SectorParser):
 
         # fix coordinates are in long/lat --> turn to lat/lon
         return [
-            f"{BS_DEFWPT_PREFIX}{BS_DEFINE_WAYPOINT} {fix[C.PROPERTIES_KEY][C.NAME_KEY]} {fix[C.GEOMETRY_KEY][gh.COORDINATES_KEY][LAT_INDEX]} {fix[C.GEOMETRY_KEY][gh.COORDINATES_KEY][LONG_INDEX]}"
+            f"{BS_DEFWPT_PREFIX}{BS_DEFINE_WAYPOINT} {fix[C.PROPERTIES_KEY][C.NAME_KEY]} {fix[C.GEOMETRY_KEY][gh.COORDINATES_KEY][0][LAT_INDEX]} {fix[C.GEOMETRY_KEY][gh.COORDINATES_KEY][0][LONG_INDEX]}"
             for fix in fixes
         ]
 

aviary/utils/geo_helper.py
@@ -49,7 +49,11 @@ class GeoHelper():
         if not as_geojson:
             return rounded
 
-        geojson[key][COORDINATES_KEY] = rounded
+        # Wrap in a nested list as per GeoJSON spec:
+        # http://wiki.geojson.org/GeoJSON_draft_version_6#Polygon
+        geojson_element = []
+        geojson_element.append(rounded)
+        geojson[key][COORDINATES_KEY] = geojson_element
         return geojson
 
 

aviary/test/conftest.py
@@ -113,9 +113,8 @@ def i_sector_geojson():
      obtained by calling geojson.dumps() on an I-shaped SectorElement."""
 
     return """
-    {"type": "FeatureCollection", "features": [{"type": "Feature", "properties": {"name": "test_sector", "type": "SECTOR", "shape": "I", "origin": [-0.1275, 51.5], "children": {"SECTOR_VOLUME": {"names": ["4878096944804515372"]}, "ROUTE": {"names": ["ASCENSION", "FALLEN"]}}}, "geometry": {}}, {"type": "Feature", "geometry": {"type": "Polygon", "coordinates": [[-0.3895, 50.6673], [-0.3992, 52.3319], [0.1442, 52.3319], [0.1345, 50.6673], [-0.3895, 50.6673]]}, "properties": {"name": "4878096944804515372", "type": "SECTOR_VOLUME", "lower_limit": 50, "upper_limit": 450, "length_nm": 100, "airway_width_nm": 20, "offset_nm": 40, "children": {}}}, {"type": "Feature", "properties": {"name": "ASCENSION", "type": "ROUTE", "children": {"FIX": {"names": ["FIYRE", "EARTH", "WATER", "AIR", "SPIRT"]}}}, "geometry": {"type": "LineString", "coordinates": [[-0.1275, 50.0017], [-0.1275, 50.6676], [-0.1275, 51.5], [-0.1275, 52.3322], [-0.1275, 52.9978]]}}, {"type": "Feature", "properties": {"name": "FALLEN", "type": "ROUTE", "children": {"FIX": {"names": ["SPIRT", "AIR", "WATER", "EARTH", "FIYRE"]}}}, "geometry": {"type": "LineString", "coordinates": [[-0.1275, 52.9978], [-0.1275, 52.3322], [-0.1275, 51.5], [-0.1275, 50.6676], [-0.1275, 50.0017]]}}, {"type": "Feature", "properties": {"name": "SPIRT", "type": "FIX"}, "geometry": {"type": "Point", "coordinates": [-0.1275, 52.9978]}}, {"type": "Feature", "properties": {"name": "AIR", "type": "FIX"}, "geometry": {"type": "Point", "coordinates": [-0.1275, 52.3322]}}, {"type": "Feature", "properties": {"name": "WATER", "type": "FIX"}, "geometry": {"type": "Point", "coordinates": [-0.1275, 51.5]}}, {"type": "Feature", "properties": {"name": "EARTH", "type": "FIX"}, "geometry": {"type": "Point", "coordinates": [-0.1275, 50.6676]}}, {"type": "Feature", "properties": {"name": "FIYRE", "type": "FIX"}, "geometry": {"type": "Point", "coordinates": [-0.1275, 50.0017]}}]}
+    {"type": "FeatureCollection", "features": [{"type": "Feature", "properties": {"name": "test_sector", "type": "SECTOR", "shape": "I", "origin": [-0.1275, 51.5], "children": {"SECTOR_VOLUME": {"names": ["4878096944804515372"]}, "ROUTE": {"names": ["ASCENSION", "FALLEN"]}}}, "geometry": {}}, {"type": "Feature", "geometry": {"type": "Polygon", "coordinates": [[[-0.3895, 50.6673], [-0.3992, 52.3319], [0.1442, 52.3319], [0.1345, 50.6673], [-0.3895, 50.6673]]]}, "properties": {"name": "4878096944804515372", "type": "SECTOR_VOLUME", "lower_limit": 50, "upper_limit": 450, "length_nm": 100, "airway_width_nm": 20, "offset_nm": 40, "children": {}}}, {"type": "Feature", "properties": {"name": "ASCENSION", "type": "ROUTE", "children": {"FIX": {"names": ["FIYRE", "EARTH", "WATER", "AIR", "SPIRT"]}}}, "geometry": {"type": "LineString", "coordinates": [[[-0.1275, 50.0017], [-0.1275, 50.6676], [-0.1275, 51.5], [-0.1275, 52.3322], [-0.1275, 52.9978]]]}}, {"type": "Feature", "properties": {"name": "FALLEN", "type": "ROUTE", "children": {"FIX": {"names": ["SPIRT", "AIR", "WATER", "EARTH", "FIYRE"]}}}, "geometry": {"type": "LineString", "coordinates": [[[-0.1275, 52.9978], [-0.1275, 52.3322], [-0.1275, 51.5], [-0.1275, 50.6676], [-0.1275, 50.0017]]]}}, {"type": "Feature", "properties": {"name": "SPIRT", "type": "FIX"}, "geometry": {"type": "Point", "coordinates": [[-0.1275, 52.9978]]}}, {"type": "Feature", "properties": {"name": "AIR", "type": "FIX"}, "geometry": {"type": "Point", "coordinates": [[-0.1275, 52.3322]]}}, {"type": "Feature", "properties": {"name": "WATER", "type": "FIX"}, "geometry": {"type": "Point", "coordinates": [[-0.1275, 51.5]]}}, {"type": "Feature", "properties": {"name": "EARTH", "type": "FIX"}, "geometry": {"type": "Point", "coordinates": [[-0.1275, 50.6676]]}}, {"type": "Feature", "properties": {"name": "FIYRE", "type": "FIX"}, "geometry": {"type": "Point", "coordinates": [[-0.1275, 50.0017]]}}]}
     """
-    # OLD: {"type": "FeatureCollection", "features": [{"type": "Feature", "properties": {"name": "test_sector", "type": "SECTOR", "shape": "I", "origin": [-0.1275, 51.5], "children": {"SECTOR_VOLUME": {"names": ["-14746818374995097"]}, "ROUTE": {"names": ["ASCENSION", "FALLEN"]}}}, "geometry": {}}, {"type": "Feature", "geometry": {"type": "Polygon", "coordinates": [[-0.2597, 51.0838], [-0.2621, 51.9161], [0.0071, 51.9161], [0.0047, 51.0838], [-0.2597, 51.0838]]}, "properties": {"name": "-14746818374995097", "type": "SECTOR_VOLUME", "lower_limit": 50, "upper_limit": 450, "length_nm": 100, "airway_width_nm": 20, "offset_nm": 40, "children": {}}}, {"type": "Feature", "properties": {"name": "ASCENSION", "type": "ROUTE", "children": {"FIX": {"names": ["FIYRE", "EARTH", "WATER", "AIR", "SPIRT"]}}}, "geometry": {"type": "LineString", "coordinates": [[-0.1275, 50.9174], [-0.1275, 51.0838], [-0.1275, 51.5], [-0.1275, 51.9161], [-0.1275, 52.0826]]}}, {"type": "Feature", "properties": {"name": "FALLEN", "type": "ROUTE", "children": {"FIX": {"names": ["SPIRT", "AIR", "WATER", "EARTH", "FIYRE"]}}}, "geometry": {"type": "LineString", "coordinates": [[-0.1275, 52.0826], [-0.1275, 51.9161], [-0.1275, 51.5], [-0.1275, 51.0838], [-0.1275, 50.9174]]}}, {"type": "Feature", "properties": {"name": "SPIRT", "type": "FIX"}, "geometry": {"type": "Point", "coordinates": [-0.1275, 52.0826]}}, {"type": "Feature", "properties": {"name": "AIR", "type": "FIX"}, "geometry": {"type": "Point", "coordinates": [-0.1275, 51.9161]}}, {"type": "Feature", "properties": {"name": "WATER", "type": "FIX"}, "geometry": {"type": "Point", "coordinates": [-0.1275, 51.5]}}, {"type": "Feature", "properties": {"name": "EARTH", "type": "FIX"}, "geometry": {"type": "Point", "coordinates": [-0.1275, 51.0838]}}, {"type": "Feature", "properties": {"name": "FIYRE", "type": "FIX"}, "geometry": {"type": "Point", "coordinates": [-0.1275, 50.9174]}}]}
 
 
 

aviary/test/unit/sector/test_route.py
@@ -57,7 +57,7 @@ def test_geojson(i_element):
     assert sorted(result[C.GEOMETRY_KEY].keys()) == sorted([gh.COORDINATES_KEY, C.TYPE_KEY])
 
     assert isinstance(result[C.GEOMETRY_KEY][gh.COORDINATES_KEY], list)
-    assert len(result[C.GEOMETRY_KEY][gh.COORDINATES_KEY]) == len(i_element.shape.fixes)
+    assert len(result[C.GEOMETRY_KEY][gh.COORDINATES_KEY][0]) == len(i_element.shape.fixes)
 
 
 def test_serialize(i_element):

aviary/test/unit/sector/test_sector_element.py
@@ -181,13 +181,14 @@ def test_boundary_geojson(i_element):
 
     # Multiple coordinate pairs are stored as a list of tuples.
     assert isinstance(result[C.GEOMETRY_KEY][gh.COORDINATES_KEY], list)
-    assert isinstance(result[C.GEOMETRY_KEY][gh.COORDINATES_KEY][0], tuple)
+    assert isinstance(result[C.GEOMETRY_KEY][gh.COORDINATES_KEY][0], list)
+    assert isinstance(result[C.GEOMETRY_KEY][gh.COORDINATES_KEY][0][0], tuple)
 
     # The I geometry contains 5 "bounding box" points (since the first and last are duplicates).
-    assert len(result[C.GEOMETRY_KEY][gh.COORDINATES_KEY]) == 5
+    assert len(result[C.GEOMETRY_KEY][gh.COORDINATES_KEY][0]) == 5
 
     # # Check the order of coordinates is correct, i.e. (longitude, latitude):
-    assert result[C.GEOMETRY_KEY][gh.COORDINATES_KEY][0] == (-0.2597, 51.0838)
+    assert result[C.GEOMETRY_KEY][gh.COORDINATES_KEY][0][0] == (-0.2597, 51.0838)
 
     assert sorted(result[C.PROPERTIES_KEY].keys()) == \
            sorted([C.NAME_KEY, C.TYPE_KEY, C.UPPER_LIMIT_KEY, C.LOWER_LIMIT_KEY,
@@ -211,16 +212,17 @@ def test_waypoint_geojson(i_element):
     assert sorted(result[C.GEOMETRY_KEY].keys()) == sorted([gh.COORDINATES_KEY, C.TYPE_KEY])
     assert result[C.GEOMETRY_KEY][C.TYPE_KEY] == C.POINT_VALUE
 
-    # A single coordinate pair is stored as a tuple.
-    assert isinstance(result[C.GEOMETRY_KEY][gh.COORDINATES_KEY], tuple)
-    assert len(result[C.GEOMETRY_KEY][gh.COORDINATES_KEY]) == 2
+    # A single coordinate pair is stored as a tuple (nested in a list).
+    assert isinstance(result[C.GEOMETRY_KEY][gh.COORDINATES_KEY], list)
+    assert isinstance(result[C.GEOMETRY_KEY][gh.COORDINATES_KEY][0], tuple)
+    assert len(result[C.GEOMETRY_KEY][gh.COORDINATES_KEY][0]) == 2
 
     assert sorted(result[C.PROPERTIES_KEY].keys()) == sorted([C.NAME_KEY, C.TYPE_KEY])
     assert result[C.PROPERTIES_KEY][C.NAME_KEY] == name.upper()
     assert result[C.PROPERTIES_KEY][C.TYPE_KEY] == C.FIX_VALUE
 
     # Check the order of coordinates is correct, i.e. (longitude, latitude):
-    assert result[C.GEOMETRY_KEY][gh.COORDINATES_KEY] == (-0.1275, 51.9161)
+    assert result[C.GEOMETRY_KEY][gh.COORDINATES_KEY][0] == (-0.1275, 51.9161)
 
 def test_geo_interface(y_element):
 

aviary/test/unit/utils/test_geo_helper.py
@@ -43,6 +43,7 @@ def test_format_coordinates(i_sector_geojson):
     result = GeoHelper.format_coordinates(json[C.FEATURES_KEY][3], key = key, float_precision = float_precision,
                                           as_geojson = False)
 
+    # Formatted coordinates are in a list of lists
     assert isinstance(result, list)
     expected = [tuple(round(num, float_precision) for num in longlat) for longlat in result]
     assert result == expected

commit d7ae8a1fa91b0b654fef331f126bb7bcfe29c9d8

Undo bug introduced in GeoJSON format fix
Fixes bug introduced in 8f6bdbce40a0a317d2495f4809f81342de9bd676

aviary/parser/bluesky_parser.py
@@ -131,7 +131,7 @@ class BlueskyParser(SectorParser):
 
         # fix coordinates are in long/lat --> turn to lat/lon
         return [
-            f"{BS_DEFWPT_PREFIX}{BS_DEFINE_WAYPOINT} {fix[C.PROPERTIES_KEY][C.NAME_KEY]} {fix[C.GEOMETRY_KEY][gh.COORDINATES_KEY][0][LAT_INDEX]} {fix[C.GEOMETRY_KEY][gh.COORDINATES_KEY][0][LONG_INDEX]}"
+            f"{BS_DEFWPT_PREFIX}{BS_DEFINE_WAYPOINT} {fix[C.PROPERTIES_KEY][C.NAME_KEY]} {fix[C.GEOMETRY_KEY][gh.COORDINATES_KEY][LAT_INDEX]} {fix[C.GEOMETRY_KEY][gh.COORDINATES_KEY][LONG_INDEX]}"
             for fix in fixes
         ]
 

aviary/utils/geo_helper.py
@@ -36,8 +36,10 @@ class GeoHelper():
         coords = list(geojson[key][COORDINATES_KEY])
 
         # Coordinates list may be nested.
+        nested = False
         while (isinstance(coords, list) and len(coords) == 1):
             coords = coords[0]
+            nested = True
 
         # Round to the given float precision, handling separately the cases of
         # a single coordinate pair versus a list of coordinate pairs.
@@ -51,8 +53,12 @@ class GeoHelper():
 
         # Wrap in a nested list as per GeoJSON spec:
         # http://wiki.geojson.org/GeoJSON_draft_version_6#Polygon
-        geojson_element = []
-        geojson_element.append(rounded)
+        if nested:
+            geojson_element = []
+            geojson_element.append(rounded)
+        else:
+            geojson_element = rounded
+
         geojson[key][COORDINATES_KEY] = geojson_element
         return geojson
 

aviary/test/conftest.py
@@ -113,8 +113,9 @@ def i_sector_geojson():
      obtained by calling geojson.dumps() on an I-shaped SectorElement."""
 
     return """
-    {"type": "FeatureCollection", "features": [{"type": "Feature", "properties": {"name": "test_sector", "type": "SECTOR", "shape": "I", "origin": [-0.1275, 51.5], "children": {"SECTOR_VOLUME": {"names": ["4878096944804515372"]}, "ROUTE": {"names": ["ASCENSION", "FALLEN"]}}}, "geometry": {}}, {"type": "Feature", "geometry": {"type": "Polygon", "coordinates": [[[-0.3895, 50.6673], [-0.3992, 52.3319], [0.1442, 52.3319], [0.1345, 50.6673], [-0.3895, 50.6673]]]}, "properties": {"name": "4878096944804515372", "type": "SECTOR_VOLUME", "lower_limit": 50, "upper_limit": 450, "length_nm": 100, "airway_width_nm": 20, "offset_nm": 40, "children": {}}}, {"type": "Feature", "properties": {"name": "ASCENSION", "type": "ROUTE", "children": {"FIX": {"names": ["FIYRE", "EARTH", "WATER", "AIR", "SPIRT"]}}}, "geometry": {"type": "LineString", "coordinates": [[[-0.1275, 50.0017], [-0.1275, 50.6676], [-0.1275, 51.5], [-0.1275, 52.3322], [-0.1275, 52.9978]]]}}, {"type": "Feature", "properties": {"name": "FALLEN", "type": "ROUTE", "children": {"FIX": {"names": ["SPIRT", "AIR", "WATER", "EARTH", "FIYRE"]}}}, "geometry": {"type": "LineString", "coordinates": [[[-0.1275, 52.9978], [-0.1275, 52.3322], [-0.1275, 51.5], [-0.1275, 50.6676], [-0.1275, 50.0017]]]}}, {"type": "Feature", "properties": {"name": "SPIRT", "type": "FIX"}, "geometry": {"type": "Point", "coordinates": [[-0.1275, 52.9978]]}}, {"type": "Feature", "properties": {"name": "AIR", "type": "FIX"}, "geometry": {"type": "Point", "coordinates": [[-0.1275, 52.3322]]}}, {"type": "Feature", "properties": {"name": "WATER", "type": "FIX"}, "geometry": {"type": "Point", "coordinates": [[-0.1275, 51.5]]}}, {"type": "Feature", "properties": {"name": "EARTH", "type": "FIX"}, "geometry": {"type": "Point", "coordinates": [[-0.1275, 50.6676]]}}, {"type": "Feature", "properties": {"name": "FIYRE", "type": "FIX"}, "geometry": {"type": "Point", "coordinates": [[-0.1275, 50.0017]]}}]}
+    {"type": "FeatureCollection", "features": [{"type": "Feature", "properties": {"name": "test_sector", "type": "SECTOR", "shape": "I", "origin": [-0.1275, 51.5], "children": {"SECTOR_VOLUME": {"names": ["4878096944804515372"]}, "ROUTE": {"names": ["ASCENSION", "FALLEN"]}}}, "geometry": {}}, {"type": "Feature", "geometry": {"type": "Polygon", "coordinates": [[[-0.3895, 50.6673], [-0.3992, 52.3319], [0.1442, 52.3319], [0.1345, 50.6673], [-0.3895, 50.6673]]]}, "properties": {"name": "4878096944804515372", "type": "SECTOR_VOLUME", "lower_limit": 50, "upper_limit": 450, "length_nm": 100, "airway_width_nm": 20, "offset_nm": 40, "children": {}}}, {"type": "Feature", "properties": {"name": "ASCENSION", "type": "ROUTE", "children": {"FIX": {"names": ["FIYRE", "EARTH", "WATER", "AIR", "SPIRT"]}}}, "geometry": {"type": "LineString", "coordinates": [[-0.1275, 50.0017], [-0.1275, 50.6676], [-0.1275, 51.5], [-0.1275, 52.3322], [-0.1275, 52.9978]]}}, {"type": "Feature", "properties": {"name": "FALLEN", "type": "ROUTE", "children": {"FIX": {"names": ["SPIRT", "AIR", "WATER", "EARTH", "FIYRE"]}}}, "geometry": {"type": "LineString", "coordinates": [[-0.1275, 52.9978], [-0.1275, 52.3322], [-0.1275, 51.5], [-0.1275, 50.6676], [-0.1275, 50.0017]]}}, {"type": "Feature", "properties": {"name": "SPIRT", "type": "FIX"}, "geometry": {"type": "Point", "coordinates": [-0.1275, 52.9978]}}, {"type": "Feature", "properties": {"name": "AIR", "type": "FIX"}, "geometry": {"type": "Point", "coordinates": [-0.1275, 52.3322]}}, {"type": "Feature", "properties": {"name": "WATER", "type": "FIX"}, "geometry": {"type": "Point", "coordinates": [-0.1275, 51.5]}}, {"type": "Feature", "properties": {"name": "EARTH", "type": "FIX"}, "geometry": {"type": "Point", "coordinates": [-0.1275, 50.6676]}}, {"type": "Feature", "properties": {"name": "FIYRE", "type": "FIX"}, "geometry": {"type": "Point", "coordinates": [-0.1275, 50.0017]}}]}
     """
+    #{"type": "FeatureCollection", "features": [{"type": "Feature", "properties": {"name": "test_sector", "type": "SECTOR", "shape": "I", "origin": [-0.1275, 51.5], "children": {"SECTOR_VOLUME": {"names": ["4878096944804515372"]}, "ROUTE": {"names": ["ASCENSION", "FALLEN"]}}}, "geometry": {}}, {"type": "Feature", "geometry": {"type": "Polygon", "coordinates": [[[-0.3895, 50.6673], [-0.3992, 52.3319], [0.1442, 52.3319], [0.1345, 50.6673], [-0.3895, 50.6673]]]}, "properties": {"name": "4878096944804515372", "type": "SECTOR_VOLUME", "lower_limit": 50, "upper_limit": 450, "length_nm": 100, "airway_width_nm": 20, "offset_nm": 40, "children": {}}}, {"type": "Feature", "properties": {"name": "ASCENSION", "type": "ROUTE", "children": {"FIX": {"names": ["FIYRE", "EARTH", "WATER", "AIR", "SPIRT"]}}}, "geometry": {"type": "LineString", "coordinates": [[[-0.1275, 50.0017], [-0.1275, 50.6676], [-0.1275, 51.5], [-0.1275, 52.3322], [-0.1275, 52.9978]]]}}, {"type": "Feature", "properties": {"name": "FALLEN", "type": "ROUTE", "children": {"FIX": {"names": ["SPIRT", "AIR", "WATER", "EARTH", "FIYRE"]}}}, "geometry": {"type": "LineString", "coordinates": [[[-0.1275, 52.9978], [-0.1275, 52.3322], [-0.1275, 51.5], [-0.1275, 50.6676], [-0.1275, 50.0017]]]}}, {"type": "Feature", "properties": {"name": "SPIRT", "type": "FIX"}, "geometry": {"type": "Point", "coordinates": [[-0.1275, 52.9978]]}}, {"type": "Feature", "properties": {"name": "AIR", "type": "FIX"}, "geometry": {"type": "Point", "coordinates": [[-0.1275, 52.3322]]}}, {"type": "Feature", "properties": {"name": "WATER", "type": "FIX"}, "geometry": {"type": "Point", "coordinates": [[-0.1275, 51.5]]}}, {"type": "Feature", "properties": {"name": "EARTH", "type": "FIX"}, "geometry": {"type": "Point", "coordinates": [[-0.1275, 50.6676]]}}, {"type": "Feature", "properties": {"name": "FIYRE", "type": "FIX"}, "geometry": {"type": "Point", "coordinates": [[-0.1275, 50.0017]]}}]}
 
 
 

aviary/test/unit/sector/test_route.py
@@ -57,7 +57,7 @@ def test_geojson(i_element):
     assert sorted(result[C.GEOMETRY_KEY].keys()) == sorted([gh.COORDINATES_KEY, C.TYPE_KEY])
 
     assert isinstance(result[C.GEOMETRY_KEY][gh.COORDINATES_KEY], list)
-    assert len(result[C.GEOMETRY_KEY][gh.COORDINATES_KEY][0]) == len(i_element.shape.fixes)
+    assert len(result[C.GEOMETRY_KEY][gh.COORDINATES_KEY]) == len(i_element.shape.fixes)
 
 
 def test_serialize(i_element):

aviary/test/unit/sector/test_sector_element.py
@@ -212,17 +212,17 @@ def test_waypoint_geojson(i_element):
     assert sorted(result[C.GEOMETRY_KEY].keys()) == sorted([gh.COORDINATES_KEY, C.TYPE_KEY])
     assert result[C.GEOMETRY_KEY][C.TYPE_KEY] == C.POINT_VALUE
 
-    # A single coordinate pair is stored as a tuple (nested in a list).
-    assert isinstance(result[C.GEOMETRY_KEY][gh.COORDINATES_KEY], list)
-    assert isinstance(result[C.GEOMETRY_KEY][gh.COORDINATES_KEY][0], tuple)
-    assert len(result[C.GEOMETRY_KEY][gh.COORDINATES_KEY][0]) == 2
+    # A single coordinate pair is stored as a tuple.
+    #assert isinstance(result[C.GEOMETRY_KEY][gh.COORDINATES_KEY], list)
+    assert isinstance(result[C.GEOMETRY_KEY][gh.COORDINATES_KEY], tuple)
+    assert len(result[C.GEOMETRY_KEY][gh.COORDINATES_KEY]) == 2
 
     assert sorted(result[C.PROPERTIES_KEY].keys()) == sorted([C.NAME_KEY, C.TYPE_KEY])
     assert result[C.PROPERTIES_KEY][C.NAME_KEY] == name.upper()
     assert result[C.PROPERTIES_KEY][C.TYPE_KEY] == C.FIX_VALUE
 
     # Check the order of coordinates is correct, i.e. (longitude, latitude):
-    assert result[C.GEOMETRY_KEY][gh.COORDINATES_KEY][0] == (-0.1275, 51.9161)
+    assert result[C.GEOMETRY_KEY][gh.COORDINATES_KEY] == (-0.1275, 51.9161)
 
 def test_geo_interface(y_element):
 